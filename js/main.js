e2c61020135b1bec635e1c1b5d3e4b9e08790a0b
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-04-27 14:56:39 +0200

    sistemazioni nomi js

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@ const firstname = prompt ("Inserisci il tuo nome");
 //chiedo all'utente il suo cognome
 const secondname = prompt ("Inserisci il tuo cognome");
 //chiedo all'utente il suo colore preferito
-const pref_color = prompt ("Inserisci il tuo colore preferito");
+const preferredColor = prompt ("Inserisci il tuo colore preferito");
 //scrivo sulla pagina nomecognomecolorepreferito21
-const pswgenerator = firstname + secondname + pref_color + "21";
+const generatedPassword = firstname + secondname + preferredColor + "21";
 //stampa su console:
 //console.log(pswgenerator) 
 //attribuisco variabile al mio id html
 const psw_created = document.getElementById("psw_created").innerHTML;
-document.getElementById("psw_created").innerHTML = `${psw_created} ${pswgenerator}`;
+document.getElementById("psw_created").innerHTML = `${psw_created} ${generatedPassword}`;
